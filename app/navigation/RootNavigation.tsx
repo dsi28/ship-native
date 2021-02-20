b9af467eae02acc1ca1891b1e7bff1f7505f4667
David <dsi28@hotmail.com>
2021-02-20 16:52:45 -0600

    basic tab bar ready

--- app/navigation/RootNavigation.tsx
+++ app/navigation/RootNavigation.tsx
@@ -1,7 +1,9 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer, Theme } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
 import { Text } from 'react-native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import PostSignup from '../screens/PostSignup';
 import SellerProfileStack from '../screens/Profile/SellerProfile';
 import UserProfile from '../screens/Profile/UserProfile';
@@ -9,21 +11,121 @@ import SellerBidsTabs from '../screens/SellerBids/tabNavigator';
 import UserRequests from '../screens/UserRequest';
 import { navigationRef } from './NavigationService';
 
-interface RootNavigatorProps {
-  theme: Theme;
-}
-
-const Stack = createStackNavigator();
-
 const TempComponent = () => (
   <>
     <Text>test</Text>
   </>
 );
 
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator
+      initialRouteName="Feed"
+      tabBarOptions={{
+        activeTintColor: '#e91e63'
+      }}
+    >
+      <Tab.Screen
+        name="Feed"
+        component={TempComponent}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="home" color={color} size={size} />
+          )
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={TempComponent}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="magnify" color={color} size={size} />
+          )
+        }}
+      />
+      <Tab.Screen
+        name="New"
+        component={TempComponent}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons
+              name="plus-circle-outline"
+              color={color}
+              size={size * 2}
+            />
+          ),
+          tabBarLabel: () => null
+        }}
+      />
+      {/*
+      https://stackoverflow.com/questions/59304281/create-custom-bottom-tab-navigator-in-react-native
+      <Tab.Screen
+        name="Add"
+        options={{
+          tabBarLabel: '',
+          tabBarIcon: ({ color }) => (
+            <View
+              style={{
+                position: 'absolute',
+                bottom: 0, // space from bottombar
+                height: 68,
+                width: 68,
+                borderRadius: 34,
+                borderWidth: 4,
+                borderColor: color,
+                justifyContent: 'center',
+                alignItems: 'center'
+              }}
+            >
+              <MaterialCommunityIcons
+                name="plus-circle-outline"
+                color={color}
+                size={68}
+              />
+            </View>
+          )
+        }}
+        component={TempComponent}
+      /> */}
+      <Tab.Screen
+        name="Chat"
+        component={TempComponent}
+        options={{
+          tabBarLabel: 'Updates',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="chat" color={color} size={size} />
+          ),
+          tabBarBadge: 3
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={TempComponent}
+        options={{
+          tabBarLabel: 'Profile',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="account" color={color} size={size} />
+          )
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+
+interface RootNavigatorProps {
+  theme: Theme;
+}
+
+const Stack = createStackNavigator();
+
 const RootNavigator: React.FC<RootNavigatorProps> = ({ theme }) => (
   <NavigationContainer ref={navigationRef} theme={theme}>
     <Stack.Navigator>
+      <Stack.Screen name="FlowStart" component={MyTabs} />
       <Stack.Screen name="SellerBids" component={SellerBidsTabs} />
       <Stack.Screen name="UserRequests" component={UserRequests} />
       <Stack.Screen
