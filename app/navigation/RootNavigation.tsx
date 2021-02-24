43aaec0ebfb87b46d0aa37cbcd798500b6bb0c2a
David <dsi28@hotmail.com>
2021-02-23 23:59:20 -0600

    update models + redux

--- app/navigation/RootNavigation.tsx
+++ app/navigation/RootNavigation.tsx
@@ -2,8 +2,6 @@ import { NavigationContainer, Theme } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
 import PostSignup from '../screens/PostSignup';
-import SellerBidsTabs from '../screens/SellerBids/tabNavigator';
-import UserRequests from '../screens/UserRequest';
 import BottomTabsNav from './BottomTabNavigation';
 import { navigationRef } from './NavigationService';
 
@@ -17,8 +15,8 @@ const RootNavigator: React.FC<RootNavigatorProps> = ({ theme }) => (
   <NavigationContainer ref={navigationRef} theme={theme}>
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="FlowStart" component={BottomTabsNav} />
-      <Stack.Screen name="SellerBids" component={SellerBidsTabs} />
-      <Stack.Screen name="UserRequests" component={UserRequests} />
+      {/* <Stack.Screen name="SellerBids" component={SellerBidsTabs} />
+      <Stack.Screen name="UserRequests" component={UserRequests} /> */}
 
       <Stack.Screen
         name="SignUp"
