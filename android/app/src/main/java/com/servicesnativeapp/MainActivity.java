ff055a811bdcb94b4df4130be7aef5ada091cf9e
David <dsi28@hotmail.com>
2021-02-23 23:47:32 -0600

    clean project

--- android/app/src/main/java/com/servicesnativeapp/MainActivity.java
+++ android/app/src/main/java/com/servicesnativeapp/MainActivity.java
@@ -1,4 +1,4 @@
-package com.servicesnativeapp;
+package com.shipnativeapp;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "ServicesNativeApp";
+    return "ShipNativeApp";
   }
 }
