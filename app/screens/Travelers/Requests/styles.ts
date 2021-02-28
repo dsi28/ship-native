0558322f25b78ab62ae7418e97fb9629a6b980fb
David <dsi28@hotmail.com>
2021-02-28 17:32:19 -0600

    modify top margin

--- app/screens/Travelers/Requests/styles.ts
+++ app/screens/Travelers/Requests/styles.ts
@@ -3,7 +3,7 @@ import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
   container: { backgroundColor: '#f3f5fa' },
   scrollContainer: {
-    marginTop: 30,
+    marginTop: 15,
     marginHorizontal: 15
     // backgroundColor: 'pink'
   }
