e17e57737888c177109ee8f6ce825c8ce95c8ba1
David <dsi28@hotmail.com>
2021-03-03 00:51:42 -0600

    move inline styles into style file for travler darkbackground component

--- app/components/Traveler/DarkBackGroundProperty/styles.ts
+++ app/components/Traveler/DarkBackGroundProperty/styles.ts
@@ -1,18 +1,10 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-  avatarView: {
-    width: 110,
-    backgroundColor: 'lightgray',
-    height: 110,
-    borderRadius: 55,
-    // borderWidth: 5,
-    // borderColor: 'lightgray',
-    borderColor: 'black',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginBottom: 10
-  },
-  travelerImage: { height: '100%', width: '100%', borderRadius: 55 }
+  container: {
+    paddingTop: 20,
+    paddingHorizontal: 25,
+    backgroundColor: '#f3f5fa'
+  }
 });
 export default styles;
