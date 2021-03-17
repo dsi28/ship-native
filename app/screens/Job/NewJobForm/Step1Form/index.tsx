9623dbff449f1dd313f345a1837f230b5112dbe6
David <dsi28@hotmail.com>
2021-03-17 09:52:35 -0500

    move drop down into its own component

--- app/screens/Job/NewJobForm/Step1Form/index.tsx
+++ app/screens/Job/NewJobForm/Step1Form/index.tsx
@@ -1,8 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Text, TextInput, View } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
 import { useDispatch, useSelector } from 'react-redux';
 import NextButton from '../../../../components/buttons/NextButton';
-import { setProfileUser } from '../../../../redux/actions/postProfile';
+import DropDownFormInput from '../../../../components/FormInputs/DropDown';
 import { AppState } from '../../../../redux/store/configureStore';
 import styles from './styles';
 
@@ -15,24 +17,130 @@ const NewJobS1: React.FC = () => {
   // @ts-ignore default does exsist not sure why this show up
   const userPostProfile = useSelector((state: AppState) => state.default);
   const dispatch = useDispatch();
-  const [nameInput, setNameInput] = useState(userPostProfile.name);
+
+  // const [nameInput, setNameInput] = useState(userPostProfile.name);
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
       <View style={styles.subContainer}>
         <View>
           <View style={styles.screenTitleContainer}>
             <Text style={styles.screenTitle}>Step 1</Text>
           </View>
+          <View
+            style={{
+              // marginTop: 20,
+              marginBottom: 40
+            }}
+          >
+            <Text
+              style={{
+                fontSize: 25,
+                fontWeight: 'bold',
+                color: 'black',
+                opacity: 0.8,
+                // width: '70%',
+                marginRight: 150
+              }}
+            >
+              Basic Details
+            </Text>
+          </View>
           <View>
             <View style={styles.screenInputContainer}>
-              <TextInput
-                style={styles.screenInput}
-                placeholder="Name.."
-                value={nameInput}
-                onChangeText={(e) => {
-                  setNameInput(e);
-                }}
+              <DropDownFormInput
+                labelText="Item Category"
+                placeholderText="Select a category"
+                itemList={[
+                  {
+                    label: 'Category 1',
+                    value: 'cat1'
+                    // icon: () => <Icon name="flag" size={18} color="#900" />,
+                    // hidden: true
+                  },
+                  {
+                    label: 'Category 2',
+                    value: 'cat2'
+                    // icon: () => <Icon name="flag" size={18} color="#900" />
+                  },
+                  {
+                    label: 'Category 3',
+                    value: 'cat3'
+                    // icon: () => <Icon name="flag" size={18} color="#900" />
+                  }
+                ]}
               />
+              <View style={{ marginBottom: 30 }}>
+                <TextInput
+                  style={styles.screenInput}
+                  placeholder="Item Name"
+                  // value={nameInput}
+                  onChangeText={(e) => {
+                    console.log(e);
+                    // setNameInput(e);
+                  }}
+                />
+              </View>
+              <View style={{ marginBottom: 30 }}>
+                <TextInput
+                  style={styles.screenInput}
+                  placeholder="Delivery Date"
+                  // value={nameInput}
+                  onChangeText={(e) => {
+                    console.log(e);
+                    // setNameInput(e);
+                  }}
+                />
+              </View>
+              <View>
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    justifyContent: 'space-between'
+                  }}
+                >
+                  <View>
+                    <Text style={{ fontSize: 20, color: 'gray' }}>
+                      Delivery Location
+                    </Text>
+                  </View>
+                  <View>
+                    <MaterialIcon
+                      name="location-pin"
+                      size={25}
+                      color="#e91e63"
+                    />
+                  </View>
+                </View>
+                <View style={{ marginBottom: 30 }}>
+                  <TextInput
+                    style={styles.screenInput}
+                    placeholder="..."
+                    // value={nameInput}
+                    onChangeText={(e) => {
+                      console.log(e);
+                      // setNameInput(e);
+                    }}
+                  />
+                </View>
+              </View>
+              <View>
+                <View>
+                  <Text style={{ fontSize: 20, color: 'gray' }}>
+                    Item Value
+                  </Text>
+                </View>
+                <View style={{ marginBottom: 30 }}>
+                  <TextInput
+                    style={styles.screenInput}
+                    placeholder="..."
+                    // value={nameInput}
+                    onChangeText={(e) => {
+                      console.log(e);
+                      // setNameInput(e);
+                    }}
+                  />
+                </View>
+              </View>
             </View>
           </View>
         </View>
@@ -41,13 +149,14 @@ const NewJobS1: React.FC = () => {
             buttonText="Next"
             onPressHandler={() => {
               // NavigationService.navigate('Email', EmailInput);
-              dispatch(setProfileUser({ ...userPostProfile, name: nameInput }));
+              // dispatch(setProfileUser({ ...userPostProfile, name: nameInput }));
             }}
-            isDisabled={nameInput.length < 2}
+            // isDisabled={nameInput.length < 2}
+            isDisabled={false}
           />
         </View>
       </View>
-    </View>
+    </ScrollView>
   );
 };
 export default NewJobS1;
--- app/screens/Job/NewJobForm/Step1Form/styles.ts
+++ app/screens/Job/NewJobForm/Step1Form/styles.ts
@@ -3,41 +3,46 @@ import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
   container: {
     height: '100%',
-    marginHorizontal: 20,
+    width: '100%',
+    paddingHorizontal: 20,
     marginVertical: 10
+    // backgroundColor: 'green'
   },
   subContainer: {
     flexDirection: 'column',
-    height: '100%'
+    height: '100%',
+    width: '100%'
+    // backgroundColor: 'pink'
   },
   screenTitleContainer: {
     // marginTop: 20,
     marginBottom: 50
   },
   screenTitle: {
-    fontSize: 40,
-    // fontWeight: 'bold',
+    fontSize: 30,
+    fontWeight: 'bold',
     color: 'black',
     // width: '70%',
     marginRight: 150
   },
   screenInputContainer: {
     borderRadius: 10
+    // zIndex: 10
     // borderColor: 'black',
     // borderWidth: 10,
   },
   screenInput: {
     fontSize: 20,
-    marginBottom: 20,
+    // marginBottom: 20,
     borderBottomWidth: 2,
-    borderBottomColor: 'black'
+    borderBottomColor: 'lightgray'
   },
   screenNextButtonContainer: {
-    marginBottom: 20,
-    bottom: 0,
-    right: 0,
-    left: 0,
-    position: 'absolute'
+    marginBottom: 20
+    // bottom: 0,
+    // right: 0,
+    // left: 0,
+    // position: 'absolute'
   }
 });
 export default styles;
