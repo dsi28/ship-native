4d090434f3f03e135395511b20ae12484f025a7f
David <dsi28@hotmail.com>
2021-05-05 22:46:59 -0500

    update login to user user redux

--- app/redux/actions/user.ts
+++ app/redux/actions/user.ts
@@ -4,16 +4,16 @@ export const NEW_USER = 'NEW_USER';
 export const SET_USER = 'SET_USER';
 export const LOG_OFF_USER = 'LOG_OFF_USER';
 
-export const newUser = (data: IUser) => ({
+export const newUserAction = (data: IUser) => ({
   type: NEW_USER,
   data
 });
 
-export const setUser = (data: IUser) => ({
+export const setUserAction = (data: IUser) => ({
   type: SET_USER,
   data
 });
 
-export const logOffUser = () => ({
+export const logOffUserAction = () => ({
   type: LOG_OFF_USER
 });
