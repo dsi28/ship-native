e8be0814672d135af4086adf3fcac217e823655a
David <dsi28@hotmail.com>
2021-05-25 23:09:23 -0500

    create search job screen files

---
+++ app/screens/Search/JobScreen/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import styles from './styles';
+
+function SearchJobScreen() {
+  // placeholder
+
+  return (
+    <ScrollView>
+      <View style={styles.container}>
+        <Text>Mangos</Text>
+      </View>
+    </ScrollView>
+  );
+}
+
+export default SearchJobScreen;
---
+++ app/screens/Search/JobScreen/styles.ts
@@ -0,0 +1,24 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  scrollViewContainer: { backgroundColor: '#f3f5fa' },
+  container: {
+    marginTop: 10,
+    marginHorizontal: 15
+    // backgroundColor: 'pink'
+  },
+  iconView: {
+    marginRight: 5,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  contentContainer: { flexDirection: 'row', justifyContent: 'space-between' },
+  filterTitle: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginHorizontal: 5,
+    color: 'black'
+  },
+  filterIconsContainer: { flexDirection: 'row', alignItems: 'center' }
+});
+export default styles;
