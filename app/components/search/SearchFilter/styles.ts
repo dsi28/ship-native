cd992b48cd859073497af560641ce1739caf0380
Elshan Samadli <elshan.samadli@gmail.com>
2021-10-25 21:29:12 -0600

    Fixed inline styles SearchFilter and FilterModal

--- app/components/search/SearchFilter/styles.ts
+++ app/components/search/SearchFilter/styles.ts
@@ -1,24 +1,13 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-  pressableNext: {
-    backgroundColor: 'black',
-    height: 60,
-    width: '100%', // substract sum of marginHorizontal(10+10)
-    borderRadius: 30,
-    // marginHorizontal: 10,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center'
-    // position: 'absolute', //If button is not sticking to the bottom of screen this has to be enabled or passed for when neede on bottom
-    // top: 20
-    // zIndex: 100,
-    // elevation: 101 // only for android?
-  },
-  pressableNextTxt: {
-    fontSize: 16,
-    fontWeight: 'bold',
-    color: 'white'
+  searchContainer: { marginBottom: 15 },
+  filterName: { fontSize: 17, fontWeight: 'bold' },
+  inputContainer: {
+    fontSize: 17,
+    borderWidth: 2,
+    borderColor: 'lightgray',
+    width: '50%'
   }
 });
 export default styles;
