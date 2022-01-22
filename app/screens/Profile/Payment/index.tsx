f9919463383c60fa76f6d3c2e8444675d9c13083
David <dsi28@hotmail.com>
2022-01-22 17:15:06 -0600

    fix gradle issues

--- app/screens/Profile/Payment/index.tsx
+++ app/screens/Profile/Payment/index.tsx
@@ -1,3 +1,4 @@
+// import type { Props as StripeProviderProps } from '@stripe/stripe-react-native/lib/typescript/src/components/StripeProvider';
 import React from 'react';
 import { Text, View } from 'react-native';
 import styles from './styles';
@@ -7,8 +8,17 @@ interface ProfilePaymentProps {
 }
 
 const ProfilePayment: React.FC<ProfilePaymentProps> = () => (
+  // @ts-ignore
+  // const StripeProvider = _StripeProvider as React.FC<StripeProviderProps>;
+
+  // <StripeProvider
+  //   publishableKey="pk_test_51KKpsMKP4EEBArik9ZSTK7asxDDTyju3EtlxR33ohAYzzrWUUawBQ0xACnsdp5ZqTcTWasVth0pJuEu5jEmHaajM00ert56ba7"
+  //   urlScheme="your-url-scheme" // required for 3D Secure and bank redirects
+  //   merchantIdentifier="merchant.com.{{YOUR_APP_NAME}}" // required for Apple Pay
+  // >
   <View style={styles.container}>
-    <Text>Payment</Text>
+    <Text>Payment yo</Text>
   </View>
+  // </StripeProvider> */}
 );
 export default ProfilePayment;
